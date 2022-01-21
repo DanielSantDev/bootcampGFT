ea8179365adecf2ad8aee1317e65486db13865e2
Daniel Santana <daniel10gja@gmail.com>
2022-01-21 17:30:06 -0300

    Tipo de dados e Variáveis

---
+++ Variáveis, tipos de dados e Operadores em Java/src/one/digitalinnovation/basecamp/main.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.basecamp;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        short s1 = 20000;
+        int i1 = 1000000000;
+        long l1 = 999999999999999999L;
+        float f1 = 4.5F;
+        double d1 = 45.787D;
+        char c1 = 'c';
+        String S1 = "String";
+        boolean boolean1 = true;
+
+    }
+
+}
