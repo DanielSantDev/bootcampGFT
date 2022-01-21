763fef90dffebdad82006dcbf53e65c844f055c7
Daniel Santana <daniel10gja@gmail.com>
2022-01-20 23:00:11 -0300

    update

---
+++ Dominando IDEs Java/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        int a = 10;
+        int b = 20;
+
+    }
+
+
+}
