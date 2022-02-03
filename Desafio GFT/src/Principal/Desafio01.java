c75ff550b5dd1d6855f97ebb5f43c6b39678b39b
Daniel Santana <daniel10gja@gmail.com>
2022-02-02 23:10:40 -0300

    Desafio01

---
+++ Desafio GFT/src/Principal/Desafio01.java
@@ -0,0 +1,19 @@
+package Principal;
+
+import java.util.Scanner;
+
+public class Desafio01 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A, B, media;
+
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+
+        media = ( A * 3.5 + B * 7.5)/11;
+
+        System.out.printf("MEDIA = %.5f", media);
+    }
+}
