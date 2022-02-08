dbb034a4e9528b556b966809f6eff4d6e85db9f3
Daniel Santana <daniel10gja@gmail.com>
2022-02-07 22:04:13 -0300

    Desafio05

--- Desafio GFT 01/src/Principal/Desafio05.java
+++ Desafio GFT 01/src/Principal/Desafio05.java
@@ -1,4 +1,17 @@
 package Principal;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Desafio05 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner leitor = new Scanner(System.in);
+
+        double n = leitor.nextDouble();
+
+        System.out.println(String.format((String.valueOf(n).startsWith("-") ? "" : "+") + "%.4E", n));
+
+    }
 }
