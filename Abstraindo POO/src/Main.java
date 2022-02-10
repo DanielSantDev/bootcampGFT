9acaa112fc10c3457af364fa8b02feb77878b405
Daniel Santana <daniel10gja@gmail.com>
2022-02-09 22:28:01 -0300

    DesafioPOO

---
+++ Abstraindo POO/src/Main.java
@@ -0,0 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso 1 java");
+        curso1.setDescricao("Descrição curso 1 Java");
+        curso1.setCargaHoraria(4);
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso 2 java");
+        curso2.setDescricao("Descrição curso 2 Java");
+        curso2.setCargaHoraria(4);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria do Curso");
+        mentoria.setData(LocalDate.now());
+        System.out.println(mentoria);
+
+    }
+
+}
