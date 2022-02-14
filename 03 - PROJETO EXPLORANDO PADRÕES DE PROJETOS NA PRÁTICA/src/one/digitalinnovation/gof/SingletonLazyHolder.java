4807c48d6a1400b6c6e9b67565eea6f1fc757159
Daniel Santana <daniel10gja@gmail.com>
2022-02-13 23:08:38 -0300

    Create SingletonLazyHolder.java

---
+++ 03 - PROJETO EXPLORANDO PADRÕES DE PROJETOS NA PRÁTICA/src/one/digitalinnovation/gof/SingletonLazyHolder.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.gof;
+
+/**
+ * Singleton "apressado"
+ */
+
+public class SingletonLazyHolder {
+
+    private static class InstanceHolder{
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+    private SingletonLazyHolder(){
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia(){
+        return InstanceHolder.instancia;
+    }
+
+}
