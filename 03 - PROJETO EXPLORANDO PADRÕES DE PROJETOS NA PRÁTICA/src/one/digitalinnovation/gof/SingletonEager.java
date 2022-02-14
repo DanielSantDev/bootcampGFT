afa27cef9d8f1ab264e167c526d32a3a2adb23cd
Daniel Santana <daniel10gja@gmail.com>
2022-02-13 23:08:41 -0300

    Create SingletonEager.java

---
+++ 03 - PROJETO EXPLORANDO PADRÕES DE PROJETOS NA PRÁTICA/src/one/digitalinnovation/gof/SingletonEager.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation.gof;
+
+/**
+ * Singleton "apressado"
+ */
+
+public class SingletonEager {
+
+     private static SingletonEager instancia = new SingletonEager();
+
+     private SingletonEager(){
+         super();
+     }
+
+     public static SingletonEager getInstance(){
+         return instancia;
+     }
+}
