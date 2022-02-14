d63aeb9be323422e5d4c69ce162892a63d581ab0
Daniel Santana <daniel10gja@gmail.com>
2022-02-14 18:10:17 -0300

    Testes

---
+++ 03 - PROJETO EXPLORANDO PADRÕES DE PROJETOS NA PRÁTICA/src/Test.java
@@ -0,0 +1,45 @@
+import one.digitalinnovation.gof.*;
+import strategy.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        //Testes relacionados ao Design Pattern Singleton
+
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+
+        //Testes relacionados ao Design Pattern Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+    }
+
+}
